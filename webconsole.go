7cfd24900b4de76f8284af8d3715e17275c0b13f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-09 16:01:46 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -1,6 +1,11 @@
 package main
 
+/*
+Web Console - lets end-users run command-line applications, cokplete with authentication and a user interface.
+*/
+
 import (
+	// Standard libraries.
 	"fmt"
 	"os"
 	"log"
@@ -10,27 +15,33 @@ import (
 	"net/http"
 )
 
+// Characters to use to generate new ID strings. Lowercase only - any user-provided IDs will be lowercased before use.
 const letters = "abcdefghijklmnopqrstuvwxyz1234567890"
 
-func generateRandomString(theLength int) string {
+// Generate a new, random 16-character ID.
+func generateID() string {
 	rand.Seed(time.Now().UnixNano())
-	result := make([]byte, theLength)
+	result := make([]byte, 16)
 	for pl := range result {
 		result[pl] = letters[rand.Intn(len(letters))]
 	}
 	return string(result)
 }
 
+// The main web server loop - the part that serves files and responds to API calls.
 func webConsole(theResponseWriter http.ResponseWriter, theRequest *http.Request) {
 	fmt.Fprintf(theResponseWriter, "Hello, %s!", theRequest.URL.Path[1:])
 }
 
+// The main body of the program - parse user-provided command-line paramaters, or start the main web server process.
 func main() {
 	http.HandleFunc("/", webConsole)
 	if len(os.Args) == 1 {
+		// If no parameters are given, simply start the web server.
 		fmt.Println("Starting web server...")
 		http.ListenAndServe(":8090", nil)
 	} else if os.Args[1] == "-list" {
+		// Print a list of existing IDs.
 		fmt.Println("List:")
 		items, err := ioutil.ReadDir(".")
 		if err != nil {
@@ -40,8 +51,9 @@ func main() {
 			fmt.Println(item.Name())
 		}
 	} else if os.Args[1] == "-generate" {
+		// Generate a new ID, and create a matching folder.
 		for {
-			newID := generateRandomString(16)
+			newID := generateID()
 			if _, err := os.Stat(newID); os.IsNotExist(err) {
 				os.Mkdir(newID, os.ModePerm)
 				fmt.Println("New ID generated: " + newID)
