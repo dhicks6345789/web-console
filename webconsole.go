82abe73141413596dfa120be99ae083bbc0e6965
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-09 13:49:15 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -6,52 +6,21 @@ import (
 	"net/http"
 )
 
+func webConsole(theResponseWriter http.ResponseWriter, theRequest *http.Request) {
+	fmt.Fprintf(theResponseWriter, "Hello, %s!", theRequest.URL.Path[1:])
+}
+
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/", webConsole)
 	if len(os.Args) == 1 {
 		http.ListenAndServe(":8090", nil)
+	} else if os.Args[1] == "-list" {
+		fmt.Println("List:")
 	}
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-}
 
 /*
-# Standard Python libraries.
-#import os
-#import re
-#import cgi
-#import hashlib
-
-print("Hello, world!")
-
-# The Flask library.
-#import flask
-
-#app = flask.Flask(__name__)
-
-#def getFile(theFilename):
-#    fileDataHandle = open(theFilename, encoding="latin-1")
-#    fileData = fileDataHandle.read()
-#    fileDataHandle.close()
-#    return(fileData)
-
-#def putFile(theFilename, theData):
-#    fileDataHandle = open(theFilename, "w", encoding="latin-1")
-#    fileDataHandle.write(fileData)
-#    fileDataHandle.close()
-
-#def runCommand(theCommand):
-#    commandHandle = os.popen(theCommand)
-#    result = commandHandle.read()
-#    commandHandle.close()
-#    return(result)
-
-#@app.route("/")
-#def api():
-#    return "Hello world!"
-
 # If a public / private key pair doesn't exist, create one.
 #@app.route("/getPublicCertificate")
 #def getPublicCertificate():
@@ -86,7 +55,4 @@ print("Hello, world!")
 #        return re.sub(".\[\d*?m", "", getFile("/var/log/build.log"))
 #    else:
 #        return getFile("/var/www/api/build.html")
-    
-#if __name__ == "__main__":
-#    app.run()
 */
