c607c8544d43a552dd48f4ea17142e8a6ca273f3
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-09 15:07:06 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -28,7 +28,7 @@ func main() {
 	} else if os.Args[1] == "-generate" {
 		newID := ""
 		for pl := 0; pl < 16; pl++ {
-			newString := newID + string("a")
+			newID = newID + string("a")
 		}
 		fmt.Println(newID)
 	}
