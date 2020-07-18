b77cb4fe4c4f03a78e49875de72a36738e6fdbd4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-18 18:36:10 +0100

    Update installWindowsService.bat

--- installWindowsService.bat
+++ installWindowsService.bat
@@ -1,6 +1,6 @@
 @echo off
 
 nssm-2.24\win64\nssm install WebConsole "C:\Program Files\WebConsole\webconsole.exe"
-nssm set WebConsole DisplayName "Web Console"
-nssm set WebConsole AppNoConsole 1
-nssm set WebConsole Start SERVICE_AUTO_START
+nssm-2.24\win64\nssm set WebConsole DisplayName "Web Console"
+nssm-2.24\win64\nssm set WebConsole AppNoConsole 1
+nssm-2.24\win64\nssm set WebConsole Start SERVICE_AUTO_START
