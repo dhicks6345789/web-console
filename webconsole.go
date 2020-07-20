bf1667e83c03e9c62e4de86733a8e3f1bbed17b1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-20 15:29:55 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -160,93 +160,97 @@ func main() {
 				if taskID == "" {
 					fmt.Fprintf(theResponseWriter, "ERROR: Missing parameter taskID.")
 				} else {
-					taskDetails := getTaskDetails(theResponseWriter, taskID)
-					authorised := false
-					authorisationError := "unknown error"
-					currentTimestamp := time.Now().Unix()
-					if token != "" {
-						if tokens[token] == 0 {
-							authorisationError = "invalid or expired token"
-						} else {
-							authorised = true
-						}
-					} else if theRequest.Form.Get("secret") == taskDetails["secret"] {								
-						authorised = true
-					} else {
-						authorisationError = "incorrect secret"
-					}
-					if authorised {
-						if token == "" {
-							token = generateIDString()
-						}
-						tokens[token] = currentTimestamp
-						// Handle Task requests.
-						if strings.HasPrefix(theRequest.URL.Path, "/task") {
-							// Serve the webconsole.html file, first adding in the Task ID value so it can be used client-side.
-							webconsoleBuffer, fileReadErr := ioutil.ReadFile("www/webconsole.html")
-							if fileReadErr == nil {
-								webconsoleString := string(webconsoleBuffer)
-								webconsoleString = strings.Replace(webconsoleString, "taskID = \"\"", "taskID = \"" + taskID + "\"", -1)
-								webconsoleString = strings.Replace(webconsoleString, "token = \"\"", "token = \"" + token + "\"", -1)
-								http.ServeContent(theResponseWriter, theRequest, "webconsole.html", time.Now(), strings.NewReader(webconsoleString))
+					taskDetails, taskErr := getTaskDetails(taskID)
+					if taskErr == nil {
+						authorised := false
+						authorisationError := "unknown error"
+						currentTimestamp := time.Now().Unix()
+						if token != "" {
+							if tokens[token] == 0 {
+								authorisationError = "invalid or expired token"
 							} else {
-								authorisationError = "couldn't read webconsole.html"
+								authorised = true
 							}
-						// API - Exchange the secret for a token.
-						} else if strings.HasPrefix(theRequest.URL.Path, "/api/getToken") {
-							fmt.Fprintf(theResponseWriter, token)
-						// API - Return the Task's title.
-						} else if strings.HasPrefix(theRequest.URL.Path, "/api/getTaskTitle") {
-							fmt.Fprintf(theResponseWriter, taskDetails["title"])
-						// API - Run a given Task.
-						} else if strings.HasPrefix(theRequest.URL.Path, "/api/runTask") {
-							if taskIsRunning(taskID) {
-								fmt.Fprintf(theResponseWriter, "OK")
-							} else {
-								commandArray := parseCommandString(taskDetails["command"])
-								var commandArgs []string
-								if len(commandArray) > 0 {
-									commandArgs = commandArray[1:]
+						} else if theRequest.Form.Get("secret") == taskDetails["secret"] {								
+							authorised = true
+						} else {
+							authorisationError = "incorrect secret"
+						}
+						if authorised {
+							if token == "" {
+								token = generateIDString()
+							}
+							tokens[token] = currentTimestamp
+							// Handle Task requests.
+							if strings.HasPrefix(theRequest.URL.Path, "/task") {
+								// Serve the webconsole.html file, first adding in the Task ID value so it can be used client-side.
+								webconsoleBuffer, fileReadErr := ioutil.ReadFile("www/webconsole.html")
+								if fileReadErr == nil {
+									webconsoleString := string(webconsoleBuffer)
+									webconsoleString = strings.Replace(webconsoleString, "taskID = \"\"", "taskID = \"" + taskID + "\"", -1)
+									webconsoleString = strings.Replace(webconsoleString, "token = \"\"", "token = \"" + token + "\"", -1)
+									http.ServeContent(theResponseWriter, theRequest, "webconsole.html", time.Now(), strings.NewReader(webconsoleString))
+								} else {
+									authorisationError = "couldn't read webconsole.html"
 								}
-								runningTasks[taskID] = exec.Command(commandArray[0], commandArgs...)
-								runningTasks[taskID].Dir = "tasks/" + taskID
-								var taskErr error
-								taskOutputs[taskID], taskErr = runningTasks[taskID].StdoutPipe()
-								if taskErr == nil {
-									taskErr = runningTasks[taskID].Start()
-								}
-								if taskErr == nil {
+							// API - Exchange the secret for a token.
+							} else if strings.HasPrefix(theRequest.URL.Path, "/api/getToken") {
+								fmt.Fprintf(theResponseWriter, token)
+							// API - Return the Task's title.
+							} else if strings.HasPrefix(theRequest.URL.Path, "/api/getTaskTitle") {
+								fmt.Fprintf(theResponseWriter, taskDetails["title"])
+							// API - Run a given Task.
+							} else if strings.HasPrefix(theRequest.URL.Path, "/api/runTask") {
+								if taskIsRunning(taskID) {
 									fmt.Fprintf(theResponseWriter, "OK")
 								} else {
-									fmt.Printf("ERROR: " + taskErr.Error())
-									fmt.Fprintf(theResponseWriter, "ERROR: " + taskErr.Error())
+									commandArray := parseCommandString(taskDetails["command"])
+									var commandArgs []string
+									if len(commandArray) > 0 {
+										commandArgs = commandArray[1:]
+									}
+									runningTasks[taskID] = exec.Command(commandArray[0], commandArgs...)
+									runningTasks[taskID].Dir = "tasks/" + taskID
+									var taskErr error
+									taskOutputs[taskID], taskErr = runningTasks[taskID].StdoutPipe()
+									if taskErr == nil {
+										taskErr = runningTasks[taskID].Start()
+									}
+									if taskErr == nil {
+										fmt.Fprintf(theResponseWriter, "OK")
+									} else {
+										fmt.Printf("ERROR: " + taskErr.Error())
+										fmt.Fprintf(theResponseWriter, "ERROR: " + taskErr.Error())
+									}
 								}
-							}
-						} else if strings.HasPrefix(theRequest.URL.Path, "/api/getTaskOutput") {
-							readBuffer := make([]byte, 10240)
-							readSize, readErr := taskOutputs[taskID].Read(readBuffer)
-							if readErr == nil {
-								fmt.Fprintf(theResponseWriter, string(readBuffer[0:readSize]))
-							} else {
-								if readErr.Error() == "EOF" {
-									delete(runningTasks, taskID)
-									delete(taskOutputs, taskID)
+							} else if strings.HasPrefix(theRequest.URL.Path, "/api/getTaskOutput") {
+								readBuffer := make([]byte, 10240)
+								readSize, readErr := taskOutputs[taskID].Read(readBuffer)
+								if readErr == nil {
+									fmt.Fprintf(theResponseWriter, string(readBuffer[0:readSize]))
+								} else {
+									if readErr.Error() == "EOF" {
+										delete(runningTasks, taskID)
+										delete(taskOutputs, taskID)
+									}
+									fmt.Fprintf(theResponseWriter, "ERROR: " + readErr.Error())
 								}
-								fmt.Fprintf(theResponseWriter, "ERROR: " + readErr.Error())
+							} else if strings.HasPrefix(theRequest.URL.Path, "/api/getTaskRunning") {
+								if taskIsRunning(taskID) {
+									fmt.Fprintf(theResponseWriter, "YES")
+								} else {
+									fmt.Fprintf(theResponseWriter, "NO")
+								}
+							} else if strings.HasPrefix(theRequest.URL.Path, "/api/keepAlive") {
+								fmt.Fprintf(theResponseWriter, "OK")
+							} else if strings.HasPrefix(theRequest.URL.Path, "/api/") {
+								fmt.Fprintf(theResponseWriter, "ERROR: Unknown API call: %s", theRequest.URL.Path)
 							}
-						} else if strings.HasPrefix(theRequest.URL.Path, "/api/getTaskRunning") {
-							if taskIsRunning(taskID) {
-								fmt.Fprintf(theResponseWriter, "YES")
-							} else {
-								fmt.Fprintf(theResponseWriter, "NO")
-							}
-						} else if strings.HasPrefix(theRequest.URL.Path, "/api/keepAlive") {
-							fmt.Fprintf(theResponseWriter, "OK")
-						} else if strings.HasPrefix(theRequest.URL.Path, "/api/") {
-							fmt.Fprintf(theResponseWriter, "ERROR: Unknown API call: %s", theRequest.URL.Path)
+						} else {
+							fmt.Fprintf(theResponseWriter, "ERROR: Not authorised - %s.", authorisationError)
 						}
 					} else {
-						fmt.Fprintf(theResponseWriter, "ERROR: Not authorised - %s.", authorisationError)
+						fmt.Fprintf(theResponseWriter, "ERROR: %s", taskErr.Error())
 					}
 				}
 			// Otherwise, try and find the static file referred to by the request URL.
