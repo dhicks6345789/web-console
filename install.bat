067da454db3238ae472d8bdcafb5b5ccbac92f8b
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-22 22:18:53 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,18 +5,22 @@ echo Installing...
 if "%1"=="" goto paramContinue
 if "%1"=="--key" (
   shift
-  set key=%1
+  set key=%2
 )
 if "%1"=="--subdomain" (
   shift
-  set subdomain=%1
+  set subdomain=%2
 )
 shift
 goto paramLoop
 :paramContinue
 
+rem Stop any existing running services.
+net stop WebConsole
+net stop TunnelTo
+
 rem Place the executables in the appropriate folder.
-erase "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
+rem erase "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
 copy webconsole.exe "C:\Program Files\WebConsole" > nul 2>&1
 copy tunnelto\tunnelto.exe "C:\Program Files\WebConsole"
 mkdir "C:\Program Files\WebConsole\www" > nul 2>&1
@@ -24,11 +28,10 @@ mkdir "C:\Program Files\WebConsole\tasks" > nul 2>&1
 xcopy /E /Y www "C:\Program Files\WebConsole\www" > nul 2>&1
 
 rem Set up the WebConsole service.
-net stop WebConsole
-nssm-2.24\win64\nssm install WebConsole "C:\Program Files\WebConsole\webconsole.exe"
-nssm-2.24\win64\nssm set WebConsole DisplayName "Web Console"
-nssm-2.24\win64\nssm set WebConsole AppNoConsole 1
-nssm-2.24\win64\nssm set WebConsole Start SERVICE_AUTO_START
+nssm-2.24\win64\nssm install WebConsole "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
+nssm-2.24\win64\nssm set WebConsole DisplayName "Web Console" > nul 2>&1
+nssm-2.24\win64\nssm set WebConsole AppNoConsole 1 > nul 2>&1
+nssm-2.24\win64\nssm set WebConsole Start SERVICE_AUTO_START > nul 2>&1
 net start WebConsole
 
 rem Allow the WbConsole service through the (local) Windows firewall.
@@ -39,9 +42,8 @@ echo %key%
 echo subdomain
 echo %subdomain%
 rem Set up the TunnelTo.dev service.
-net stop TunnelTo
 nssm-2.24\win64\nssm install TunnelTo "C:\Program Files\WebConsole\tunnelto.exe" --port 8090 --key %key% --subdomain %subdomain%
-nssm-2.24\win64\nssm set TunnelTo DisplayName "TunnelTo.dev"
-nssm-2.24\win64\nssm set TunnelTo AppNoConsole 1
-nssm-2.24\win64\nssm set TunnelTo Start SERVICE_AUTO_START
+nssm-2.24\win64\nssm set TunnelTo DisplayName "TunnelTo.dev" > nul 2>&1
+nssm-2.24\win64\nssm set TunnelTo AppNoConsole 1 > nul 2>&1
+nssm-2.24\win64\nssm set TunnelTo Start SERVICE_AUTO_START > nul 2>&1
 net start TunnelTo
