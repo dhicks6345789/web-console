709ad1aa0514aa4eb63efe5d35dd1a5fee5ceaab
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-22 20:42:38 +0100

    Update and rename installWindowsService.bat to install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,15 @@
 @echo off
 
+echo Installing...
+
+rem Place the executables in the appropriate folder.
+erase "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
+copy webconsole.exe "C:\Program Files\WebConsole" > nul 2>&1
+copy tunnelto\tunnelto.exe "C:\Program Files\WebConsole"
+mkdir "C:\Program Files\WebConsole\www" > nul 2>&1
+mkdir "C:\Program Files\WebConsole\tasks" > nul 2>&1
+xcopy /E /Y www "C:\Program Files\WebConsole\www" > nul 2>&1
+
 rem Set up the WebConsole service.
 net stop WebConsole
 nssm-2.24\win64\nssm install WebConsole "C:\Program Files\WebConsole\webconsole.exe"
@@ -8,5 +18,5 @@ nssm-2.24\win64\nssm set WebConsole AppNoConsole 1
 nssm-2.24\win64\nssm set WebConsole Start SERVICE_AUTO_START
 net start WebConsole
 
-rem Allow the WbConsole service through the firewall.
+rem Allow the WbConsole service through the (local) Windows firewall.
 netsh.exe advfirewall firewall add rule name="WebConsole" program="C:\Program Files\WebConsole\webconsole.exe" protocol=tcp dir=in enable=yes action=allow profile="private,domain,public"
