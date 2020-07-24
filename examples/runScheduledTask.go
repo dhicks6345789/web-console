b830c6b7fc3a9887f320ac8eb31b96d75216037f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-24 14:01:37 +0100

    Update runScheduledTask.go

--- examples/runScheduledTask.go
+++ examples/runScheduledTask.go
@@ -2,5 +2,22 @@
 // Doesn't exit until the schedualed task has finished, and prints out a status indicator as the task is running.
 // Uses previous run times to guess the total time the shedualed task will take.
 
+import (
+	// Standard libraries.
+	"time"
+	"os/exec"
+)
+
+theCommand = exec.Command("C:\Windows\System32\schtasks.exe", "/RUN", "/TN", "Salamander - Diary")
+taskOutput, taskErr := theCommand.StdoutPipe()
+if taskErr == nil {
+	taskErr = theCommand.Start()
+}
+if taskErr == nil {
+	println("OK")
+} else {
+	println("ERROR: " + taskErr.Error())
+}
+
 // C:\Windows\System32\schtasks.exe /RUN /TN "Salamander - Diary"
 // C:\Windows\System32\schtasks.exe /QUERY /TN "Salamander - Diary" /FO CSV /NH
