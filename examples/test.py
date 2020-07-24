6660e7a9d0d0af8b6c232262678db8a1c56fa448
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-24 13:19:47 +0100

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -1,10 +1,14 @@
+# A basic Python 3 test script that simply prints a sentence a word at a time, with a pause between each word.
+# Simulates a script that takes a few seconds to complete. Outputs progress information for the user which is displayed as a progress bar by Web Console.
+
 import sys
 import time
 
-wordArray = ["The","Quick","Brown","Fox","Jumped","Over","The","Lazy","Dog"]
+sentence = ["The","Quick","Brown","Fox","Jumped","Over","The","Lazy","Dog"]
+wordArray = sentence.split(" ")
 
 for pl in range(0, len(wordArray)):
-print (wordArray[pl])
-    print ("Progress: Progress " + str(int(round(pl / (len(wordArray)-1), 2) * 100)))
-    sys.stdout.flush()
-    time.sleep(2)
+	print (wordArray[pl])
+	print ("Progress: Progress " + str(int(round(pl / (len(wordArray)-1), 2) * 100)))
+	sys.stdout.flush()
+	time.sleep(2)
