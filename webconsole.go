4a3b0d5a92625ec6565eb40a9beccc687cb357de
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-07-30 16:03:09 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -396,7 +396,7 @@ func main() {
 								if _, runningTaskFound := runningTasks[taskID]; !runningTaskFound {
 									if taskDetails["progress"] == "Y" {
 										fmt.Fprintf(theResponseWriter, "Progress: Progress 100\n")
-										fmt.Fprintf(theResponseWriter, "Done - runtime %d seconds.\n", taskRunTimes[taskID])
+										fmt.Fprintf(theResponseWriter, "Done - runtime %d seconds.\n",  taskStopTimes[taskID] - taskStartTimes[taskID])
 									}
 									fmt.Fprintf(theResponseWriter, "ERROR: EOF")
 									//delete(taskOutputs, taskID)
