a12e7792096857ed7df341064eebf162afef9b05
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-10-08 13:48:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ go get golang.org/x/crypto/bcrypt
 go get github.com/360EntSecGroup-Skylar/excelize
 go build webconsole.go
 cp webconsole /usr/local/bin
-mkdir /etc/webconsole
+[ -d /etc/webconsole ] && echo mkdir /etc/webconsole
 cp --recursive www /etc/webconsole
