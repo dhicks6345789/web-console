4b0b54ecf3b3ca9de22fbb1234ec43129d7d4e84
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-06 18:57:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ go build webconsole.go
 cp webconsole /usr/local/bin
 [ ! -d /etc/webconsole ] && mkdir /etc/webconsole
 cp --recursive www /etc/webconsole
+
+# Set up Webconsole as a systemd service - first, stop any existing Webconsole service...
+systemctl stop webconsole
+# ...then set up systemd to run Webconsole.
+cp webconsole.service /etc/systemd/system/webconsole.service
+chmod /etc/systemd/system/webconsole.service 644
+systemctl start webconsole
+systemctl enable webconsole
