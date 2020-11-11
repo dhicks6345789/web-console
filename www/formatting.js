a1716eed7f5e08de07c476adb9d20fd4f5b35fab
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-11 12:52:43 +0000

    Update formatting.js

--- www/formatting.js
+++ www/formatting.js
@@ -1,5 +1,11 @@
 								// If a string starts with "ERROR:", format the line in red.
-								if (value.toLowerCase().startsWith("error:")) {
+								if (value.toLowerCase().startsWith("error")) {
 									value = "<div style='color:red'>" + value + "</div>"
+									$("#taskAlerts").html(value);
+								} else if (value.toLowerCase().startsWith("warning") || value.toLowerCase().startsWith("alert")) {
+									value = "<div style='color:yellow'>" + value + "</div>"
+									$("#taskAlerts").html(value);
+								} else if (value.toLowerCase().startsWith("status")) {
+									value = "<div style='color:green'>" + value + "</div>"
 									$("#taskAlerts").html(value);
 								}
