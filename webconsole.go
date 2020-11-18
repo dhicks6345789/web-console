6e7b6bc6ec0be20740edc9b60956491d5542bdbe
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-18 11:18:44 +0000

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -647,9 +647,11 @@ func main() {
 										r, g, b, a := faviconImage.At(silhouetteX, silhouetteY).RGBA()
 										//if r < 128 || g < 128 || b < 128 || a < 128 {
 										if r > 128 || g > 128 || b > 128 || a > 128 {
-											silhouetteImage.Set(silhouetteX, silhouetteY, color.RGBA{255, 255, 255, 0})
-										} else {
+											//silhouetteImage.Set(silhouetteX, silhouetteY, color.RGBA{255, 255, 255, 0})
 											silhouetteImage.Set(silhouetteX, silhouetteY, color.RGBA{0, 0, 0, 255})
+										} else {
+											silhouetteImage.Set(silhouetteX, silhouetteY, color.RGBA{255, 255, 255, 0})
+											//silhouetteImage.Set(silhouetteX, silhouetteY, color.RGBA{0, 0, 0, 255})
 										}
 									}
 								}
