522f693e3cfaa2924364c68ac1b32f2f21fd5d9c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-20 18:19:43 +0000

    Update formatting.js

--- www/formatting.js
+++ www/formatting.js
@@ -1,11 +1,17 @@
 								// If a string starts with "ERROR:", format the line in red.
 								if (value.toLowerCase().startsWith("error: ")) {
 									value = "<span style='color:red'>" + value + "</span>"
-									$("#taskAlerts").html(value);
+									if (displayAlerts == true) {
+										$("#taskAlerts").html(value);
+									}
 								} else if (value.toLowerCase().startsWith("warning: ") || value.toLowerCase().startsWith("alert: ")) {
 									value = "<span style='color:yellow'>" + value + "</span>"
-									$("#taskAlerts").html(value);
+									if (displayAlerts == true) {
+										$("#taskAlerts").html(value);
+									}
 								} else if (value.toLowerCase().startsWith("status: ")) {
 									value = "<span style='color:green'>" + value.substr(8); + "</span>"
-									$("#taskAlerts").html(value);
+									if (displayAlerts == true) {
+										$("#taskAlerts").html(value);
+									}
 								}
