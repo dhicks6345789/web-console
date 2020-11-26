5d0f60b60e2d600ddae7daefdffa3f14fca2e16e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-26 22:54:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,19 @@ curl https://www.sansay.co.uk/binaries/web-console/linux-amd64/webconsole -o /us
 chmod u+x /usr/local/bin/webconsole
 [ ! -d /etc/webconsole ] && mkdir /etc/webconsole
 [ ! -d /etc/webconsole/www ] && mkdir /etc/webconsole/www
-curl https://www.sansay.co.uk/binaries/web-console/www/browserconfig.xml -o /etc/webconsole/browserconfig.xml
-curl https://www.sansay.co.uk/binaries/web-console/www/copyIcon.svg -o /etc/webconsole/copyIcon.svg
-curl https://www.sansay.co.uk/binaries/web-console/www/favicon.png -o /etc/webconsole/favicon.png
-curl https://www.sansay.co.uk/binaries/web-console/www/formatting.js -o /etc/webconsole/formatting.js
-curl https://www.sansay.co.uk/binaries/web-console/www/index.html -o /etc/webconsole/index.html
-curl https://www.sansay.co.uk/binaries/web-console/www/site.webmanifest -o /etc/webconsole/site.webmanifest
-curl https://www.sansay.co.uk/binaries/web-console/www/webconsole.html -o /etc/webconsole/webconsole.html
+curl https://www.sansay.co.uk/webconsole/www/browserconfig.xml -o /etc/webconsole/browserconfig.xml
+curl https://www.sansay.co.uk/webconsole/www/copyIcon.svg -o /etc/webconsole/copyIcon.svg
+curl https://www.sansay.co.uk/webconsole/www/favicon.png -o /etc/webconsole/favicon.png
+curl https://www.sansay.co.uk/webconsole/www/formatting.js -o /etc/webconsole/formatting.js
+curl https://www.sansay.co.uk/webconsole/www/index.html -o /etc/webconsole/index.html
+curl https://www.sansay.co.uk/webconsole/www/site.webmanifest -o /etc/webconsole/site.webmanifest
+curl https://www.sansay.co.uk/webconsole/www/webconsole.html -o /etc/webconsole/webconsole.html
 [ ! -d /etc/webconsole/www/favicons ] && mkdir /etc/webconsole/www/favicons
-curl https://www.sansay.co.uk/binaries/web-console/www/favicons/apple.html -o /etc/webconsole/favicons/apple.html
-curl https://www.sansay.co.uk/binaries/web-console/www/favicons/banana.html -o /etc/webconsole/favicons/banana.html
-#curl https://www.sansay.co.uk/binaries/web-console/www/favicons/.html /etc/webconsole/favicons/.html
+curl https://www.sansay.co.uk/binaries/webconsole/www/favicons/apple.html -o /etc/webconsole/favicons/apple.html
+curl https://www.sansay.co.uk/binaries/webconsole/www/favicons/banana.html -o /etc/webconsole/favicons/banana.html
 
 # ...set up systemd to run Webconsole.
-curl https://www.sansay.co.uk/binaries/web-console/webconsole.service -o /etc/systemd/system/webconsole.service
+curl https://www.sansay.co.uk/webconsole/webconsole.service -o /etc/systemd/system/webconsole.service
 chmod 644 /etc/systemd/system/webconsole.service
 systemctl start webconsole
 systemctl enable webconsole
