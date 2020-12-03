0292e96d28288d612ee5389eaf8ce7288154089e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-12-03 16:15:46 +0000

    Create reformatOutput.py

---
+++ examples/reformatOutput.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+
+def replaceAll(theString, theMatch, theReplace):
+	newString = theString.replace(theMatch, theReplace)
+	while not newString == theString:
+		theString = newString
+		newString = theString.replace(theMatch, theReplace)
+	return newString
+
+for line in sys.stdin:
+	line = line.strip()
+	line = replaceAll(line, "  ", " ")
+	line = replaceAll(line, " |", ":")
+	if line.startswith("WARN "):
+		print(line.replace("WARN ","WARNING: "))
+	elif line.startswith("* branch"):
+		print(line.replace("* branch","Branch:"))
+	else:
+		if not (line.startswith("| EN") or line.startswith("---") or line.startswith("Start building sites") or line == ""):
+			print(line)
