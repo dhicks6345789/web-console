946173cf365a1d947cbfde5649f15be471fdf0bb
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-12-03 12:32:36 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,10 @@
 @echo off
-if "%1"=="" (
-  echo How to use...
-  goto end
-)
+rem if "%1"=="" (
+rem   echo How to use...
+rem   goto end
+rem )
 
 echo Installing...
-
-exit /b 0
 
 set key=
 set subdomain=
@@ -31,18 +29,20 @@ net stop WebConsole > nul 2>&1
 net stop TunnelTo > nul 2>&1
 
 rem Place the executables in the appropriate folder.
-copy webconsole.exe "C:\Program Files\WebConsole" > nul 2>&1
-copy tunnelto\tunnelto.exe "C:\Program Files\WebConsole" > nul 2>&1
-mkdir "C:\Program Files\WebConsole\www" > nul 2>&1
-mkdir "C:\Program Files\WebConsole\tasks" > nul 2>&1
-xcopy /E /Y www "C:\Program Files\WebConsole\www" > nul 2>&1
+copy webconsole.exe "C:\Program Files\WebConsole"
+rem copy tunnelto\tunnelto.exe "C:\Program Files\WebConsole" > nul 2>&1
+rem mkdir "C:\Program Files\WebConsole\www" > nul 2>&1
+rem mkdir "C:\Program Files\WebConsole\tasks" > nul 2>&1
+rem xcopy /E /Y www "C:\Program Files\WebConsole\www" > nul 2>&1
 
 rem Set up the WebConsole service.
-nssm-2.24\win64\nssm install WebConsole "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
-nssm-2.24\win64\nssm set WebConsole DisplayName "Web Console" > nul 2>&1
-nssm-2.24\win64\nssm set WebConsole AppNoConsole 1 > nul 2>&1
-nssm-2.24\win64\nssm set WebConsole Start SERVICE_AUTO_START > nul 2>&1
+rem nssm-2.24\win64\nssm install WebConsole "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
+rem nssm-2.24\win64\nssm set WebConsole DisplayName "Web Console" > nul 2>&1
+rem nssm-2.24\win64\nssm set WebConsole AppNoConsole 1 > nul 2>&1
+rem nssm-2.24\win64\nssm set WebConsole Start SERVICE_AUTO_START > nul 2>&1
 net start WebConsole
+
+exit /b 0
 
 rem Allow the WebConsole service through the (local) Windows firewall.
 netsh.exe advfirewall firewall add rule name="WebConsole" program="C:\Program Files\WebConsole\webconsole.exe" protocol=tcp dir=in enable=yes action=allow profile="private,domain,public" > nul 2>&1
