6b3c49f0488954e1ebf4325f27d5fc23d1828fce
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-12-03 15:34:14 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 
+net stop WebConsole
+erase "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
 erase webconsole.exe > nul 2>&1
+
 go get github.com/nfnt/resize
 go get github.com/dennwc/gotrace
 go get github.com/kodeworks/golang-image-ico
@@ -8,10 +11,9 @@ go get golang.org/x/crypto/bcrypt
 go get github.com/360EntSecGroup-Skylar/excelize
 go build webconsole.go
 
-rem call install.bat --key Usff7rA5eSFtno9kpn9GSP --subdomain somethinggoeshere
+copy webconsole.exe "C:\Program Files\WebConsole"
+net start WebConsole
+
+rem call install.bat --key somekey --subdomain somethinggoeshere
 rem call install.bat --go
 rem net start TunnelTo
-
-rem echo Running...
-rem cd "C:\Program Files\WebConsole"
-rem webconsole 2>&1
