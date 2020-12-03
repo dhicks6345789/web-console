2014d31145aaf9c89044382e56205610e2c45c06
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-12-03 13:07:27 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -33,7 +33,10 @@ echo Downloading Web Console v%VERSION% binary...
 powershell -command "& {&'Invoke-WebRequest' -Uri https://github.com/dhicks6345789/web-console/releases/download/v%VERSION%/win-amd64.exe -OutFile 'C:\Program Files\WebConsole\webconsole.exe'}"
 echo Downloading Web Console v%VERSION% support files...
 powershell -command "& {&'Invoke-WebRequest' -Uri https://github.com/dhicks6345789/web-console/archive/v%VERSION%.zip -OutFile supportFiles.zip}"
-rem xcopy /E /Y www "C:\Program Files\WebConsole\www" > nul 2>&1
+powershell -command "Expand-Archive -Path supportFiles.zip"
+xcopy /E /Y supportFiles\web-console-%VERSION%\www "C:\Program Files\WebConsole\www" > nul 2>&1
+erase supportFiles.zip
+deltree /Y supportFiles
 
 rem copy tunnelto\tunnelto.exe "C:\Program Files\WebConsole" > nul 2>&1
 rem mkdir "C:\Program Files\WebConsole\www" > nul 2>&1
