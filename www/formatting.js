bb018901aec26f815f194d8b1b6d75be6f4e6b29
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-12-09 14:19:26 +0000

    Update formatting.js

--- www/formatting.js
+++ www/formatting.js
@@ -1,17 +1,22 @@
 								// If a string starts with "ERROR:", format the line in red.
 								if (value.toLowerCase().startsWith("error: ")) {
-									value = "<span style='color:red'>" + value + "</span>"
+									value = "<span style='color:red'>" + value.substr(7) + "</span>"
 									if (displayAlerts == true) {
 										$("#taskAlerts").html(value);
 									}
 								} else if (value.toLowerCase().startsWith("warning: ") || value.toLowerCase().startsWith("alert: ")) {
-									value = "<span style='color:yellow'>" + value + "</span>"
+									value = "<span style='color:yellow'>" + value.substr(9) + "</span>"
 									if (displayAlerts == true) {
 										$("#taskAlerts").html(value);
 									}
 								} else if (value.toLowerCase().startsWith("status: ")) {
-									value = "<span style='color:green'>" + value.substr(8); + "</span>"
+									value = "<span style='color:green'>" + value.substr(8) + "</span>"
 									if (displayAlerts == true) {
 										$("#taskAlerts").html(value);
+									}
+								} else if (value.toLowerCase().startsWith("result: ")) {
+									value = "<span style='color:black'>" + value.substr(8) + "</span>"
+									if (displayAlerts == true) {
+										$("#taskResults").html($("#taskResults").html() + value);
 									}
 								}
