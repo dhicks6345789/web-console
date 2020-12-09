e537adddac19c32bc4da18af399b22e01941555d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-12-09 13:57:31 +0000

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -1,21 +1,27 @@
-# A basic Python 3 test script that simply prints a sentence a word at a time, with a pause between each word, simulating a script that takes a few seconds to complete.
-# By default, outputs progress information for the user which is displayed as a progress bar by Web Console, but that can be turned off if you want Web Console to provide
-# the progress bar instead.
+# A basic Python 3 test script that produces some example out, with a pause between lines to simulate work being done.
+# By default, outputs progress information for the user which is displayed as a progress bar by Web Console, but that
+# can be turned off if you want Web Console to provide the progress bar instead.
 
 import sys
 import time
 
-sentence = "The Quick Brown Fox Jumps Over The Lazy Dog"
-wordArray = sentence.split(" ")
+keywordsArray = ["STATUS","WARNING","ERROR","RESULT","PROGRESS"]
+
+outputLength = 0
+outputArray = ["STATUS: Starting...", "RESULT: To the batmobile, let's go!", "Running...", "More running...", "Yet more running...", "ERROR: No Batkeys!", "STATUS: Keys found in Batpocket!", "Fumbling with batkeys...", "RESULT: Atomic batteries to power...", "Efficiency: 73%.", "RESULT: Turbines to speed...", "RESULT: Roger, ready to move out.", "STATUS: Done."]
+for pl in range(0, len(outputArray)):
+	if outputArray[pl].split(":")[0] in keywordsArray:
+		outputLength = outputLength + 1
 
 displayProgress = True
 if len(sys.argv) > 1:
 	if sys.argv[1] == "--NOPROGRESS":
 		displayProgress = False
 
-for pl in range(0, len(wordArray)):
-	print (wordArray[pl])
+for pl in range(0, len(outputArray)):
+	print (outputArray[pl])
 	if displayProgress:
-		print ("Progress: Progress " + str(int(round(pl / (len(wordArray)-1), 2) * 100)) + "%")
+		print("PROGRESS: Progress " + str(int(round(pl / outputLength), 2) * 100) + "%")
 	sys.stdout.flush()
-	time.sleep(2)
+	if outputArray[pl].split(":")[0] in keywordsArray:
+		time.sleep(2)
