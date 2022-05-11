4e91ea42b2540bc0f02a2704a985a87d1031806b
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-05-11 13:19:17 +0100

    Update formatting.js

--- www/formatting.js
+++ www/formatting.js
@@ -2,22 +2,22 @@
 								if (value.toLowerCase().startsWith("error: ")) {
 									value = "<span style='color:Red'>" + value.substr(7) + "</span>"
 									if (displayAlerts == true) {
-										document.getElementById("taskAlerts").html(value);
+										document.getElementById("taskAlerts").innerHTML = value;
 									}
 								} else if (value.toLowerCase().startsWith("warning: ") || value.toLowerCase().startsWith("alert: ")) {
 									value = "<span style='color:Yellow'>" + value.substr(9) + "</span>"
 									if (displayAlerts == true) {
-										document.getElementById("taskAlerts").html(value);
+										document.getElementById("taskAlerts").innerHTML = value;
 									}
 								} else if (value.toLowerCase().startsWith("status: ")) {
 									value = "<span style='color:Green'>" + value.substr(8) + "</span>"
 									if (displayAlerts == true) {
-										document.getElementById("taskAlerts").html(value);
+										document.getElementById("taskAlerts").innerHTML = value;
 									}
 								} else if (value.toLowerCase().startsWith("result: ")) {
 									value = "<span style='color:Black'>" + value.substr(8) + "</span>"
 									if (displayAlerts == true) {
-										document.getElementById("taskResults").html(document.getElementById("taskResults").html() + "<div>" + value + "</div>");
+										document.getElementById("taskResults").innerHTML = document.getElementById("taskResults").innerHTML() + "<div>" + value + "</div>";
 									}
 								} else if (!value.toLowerCase().startsWith("progress: ")) {
 									value = "<span style='color:LightGray'>" + value + "</span>"
