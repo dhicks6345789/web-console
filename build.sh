766a8ed840abd39eb33f7b5beef11ce8511823ee
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-05-25 15:48:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 # Build script for WebConsole. Tested on Debian, x86 and Raspberry Pi.
 
 source VERSION
+CURRENTDATE=`date +"%D-%H:%M"`
+BUILDVERSION="$VERSION-local-$CURRENTDATE"
 
 echo Building Web Console...
 
@@ -12,7 +14,7 @@ go get github.com/dennwc/gotrace
 go get github.com/kodeworks/golang-image-ico
 go get golang.org/x/crypto/bcrypt
 go get github.com/360EntSecGroup-Skylar/excelize
-go build -ldflags "-X main.releaseVersion=$VERSION" webconsole.go
+go build -ldflags "-X main.buildVersion=$BUILDVERSION" webconsole.go
 cp webconsole /usr/local/bin
 
 # Create the application's data folder and copy the default data files into it.
