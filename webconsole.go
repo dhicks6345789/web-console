81f4526ca11d7d6aaf5e9a5833ae4d0317a884e4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-05-31 17:32:46 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -542,8 +542,9 @@ func main() {
 										for _, batchExtension := range []string{".bat", ".btm", ".cmd"} {
 											// If the command is a Windows batch file, we need to run the Windows command shell for it to execute.
 											if strings.HasSuffix(strings.ToLower(commandArray[0]), batchExtension) {
-												commandArray := parseCommandString("cmd /c " + taskDetails["command"])
+												commandArray = parseCommandString("cmd /c " + taskDetails["command"])
 												fmt.Println("2commandArray[0]: " + commandArray[0])
+												fmt.Println(commandArray)
 											}
 										}
 										var commandArgs []string
