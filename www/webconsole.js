c2fc8ca44e65c2672ed57b73cc17689e8d3d8098
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-07-10 14:51:43 +0100

    Update webconsole.js

--- www/webconsole.js
+++ www/webconsole.js
@@ -1,32 +1,38 @@
-function doAPICall(theMethod, theParams, theSuccessFunction, callMethod="POST") {
-    theParams["taskID"] = taskID;
-    theParams["token"] = token;
-    var apiCall = new XMLHttpRequest();
-    apiCall.onreadystatechange = function() {
-        if (apiCall.readyState == 4 && apiCall.status == 200) {
-            theSuccessFunction(apiCall.responseText);
+var webconsole = {
+    APICall: function(theMethod, theParams, theSuccessFunction, callMethod="POST") {
+        theParams["taskID"] = taskID;
+        theParams["token"] = token;
+        var apiCall = new XMLHttpRequest();
+        apiCall.onreadystatechange = function() {
+            if (apiCall.readyState == 4 && apiCall.status == 200) {
+                theSuccessFunction(apiCall.responseText);
+            }
         }
+        URLEncodedParams = "";
+        for (const [paramKey, paramValue] of Object.entries(theParams)) {
+            URLEncodedParams = URLEncodedParams + encodeURIComponent(paramKey) + "=" + encodeURIComponent(paramValue) + "&";
+        }
+        URLEncodedParams = URLEncodedParams.slice(0, -1);        
+        if (callMethod == "GET") {
+            apiCall.open("GET", "api/" + theMethod + "?" + URLEncodedParams, true);
+            apiCall.send();
+        } else {
+            apiCall.open("POST", "api/" + theMethod, true);
+            apiCall.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            apiCall.send(URLEncodedParams);
+        }
+    },
+    
+    APITask: function(theTaskID, thePollPeriod) {
+        console.log("Call API Task...");
+    },
+    
+    numberElementIDs: function(theNode, theNumber) {
+        if (theNode.id != undefined && theNode.id != "") {
+            theNode.id = theNode.id + "-" + theNumber;
+        }
+        theNode.childNodes.forEach(function(childNode) {
+            numberElementIDs(childNode, theNumber);
+        });
     }
-    URLEncodedParams = "";
-    for (const [paramKey, paramValue] of Object.entries(theParams)) {
-        URLEncodedParams = URLEncodedParams + encodeURIComponent(paramKey) + "=" + encodeURIComponent(paramValue) + "&";
-    }
-    URLEncodedParams = URLEncodedParams.slice(0, -1);        
-    if (callMethod == "GET") {
-        apiCall.open("GET", "api/" + theMethod + "?" + URLEncodedParams, true);
-        apiCall.send();
-    } else {
-        apiCall.open("POST", "api/" + theMethod, true);
-        apiCall.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        apiCall.send(URLEncodedParams);
-    }
-}
-
-function numberElementIDs(theNode, theNumber) {
-    if (theNode.id != undefined && theNode.id != "") {
-        theNode.id = theNode.id + "-" + theNumber;
-    }
-    theNode.childNodes.forEach(function(childNode) {
-        numberElementIDs(childNode, theNumber);
-    });
-}
+};
