311fdffa44ac03f665813ed27daaadc8ced5768c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-07-11 16:14:48 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -1090,7 +1090,7 @@ func main() {
 					debug("Asked for webroot file: " + localFilePath)
 					if _, err := os.Stat(localFilePath); errors.Is(err, os.ErrNotExist) {
 						logLine("Not found")
-						theResponseWriter.WriteHeader(http.StatusNotFound)
+						//theResponseWriter.WriteHeader(http.StatusNotFound)
 						http.ServeFile(theResponseWriter, theRequest, arguments["webroot"] + "/404.html")
 					} else {
 						logLine("Found")
