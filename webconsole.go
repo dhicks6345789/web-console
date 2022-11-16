d1a67be8f718186ba5d96db41394fe9ea0579ed7
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-11-16 14:31:36 +0000

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -454,6 +454,7 @@ func readUserFile(theConfigPath string, theHashKey string) map[string]string {
 						} else {
 							// Generate Argon2i hash.
 							// argon2.argon2_hash(userEmailAddress.strip().lower(), salt=apiKey, t=16, m=8, p=1, buflen=16, argon_type=argon2.Argon2Type.Argon2_i).hex()
+							debug("Using hash key: " + theHashKey)
 							hashedEmailAddress = hex.EncodeToString(argon2.IDKey([]byte(emailAddress), []byte(theHashKey), argon2Iterations, argon2Memory, argon2Parallelism, argon2KeyLength))
 						}
 					}
