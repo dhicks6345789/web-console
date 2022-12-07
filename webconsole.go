82bc105c012c1243ce9e482888eeef25504d56a1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-12-07 13:20:16 +0000

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -729,8 +729,8 @@ func main() {
 									defer mystartResult.Body.Close()
 									mystartJSON := new(mystartStruct)
 									mystartJSONResult := json.NewDecoder(mystartResult.Body).Decode(mystartJSON)
-									fmt.Println("mtstartJSONResult:")
-									fmt.Println(mystartJSONResult)
+									fmt.Println("mystartJSON:")
+									fmt.Println(mystartJSON)
 									if mystartJSONResult == nil {
 										if mystartJSON.Login == "valid" {
 											// Okay - we've authenticated the user, now we need to check authorisation.
