c38e57f5e9b426fb212db1e27afb5ed13d75717d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-12-08 17:13:44 +0000

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -359,6 +359,7 @@ func getTaskPermission(webConsoleRoot string, taskDetails map[string]string, mys
 				if taskDetails["taskID"] == "/" {
 					mystartUsersPath = webConsoleRoot + "/" + taskDetailValue
 				}
+				fmt.Println("Looking for file: " + mystartUsersPath)
 				if _, err := os.Stat(mystartUsersPath); !os.IsNotExist(err) {
 					fmt.Println("Looking at file: " + mystartUsersPath)
 					mystartUsers := readUserFile(mystartUsersPath, arguments["mystart" + mystartName + "APIKey"])
