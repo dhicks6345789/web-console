11652c9c72dbc9ded74fce1935f4306ca7a9b639
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-01-05 16:11:36 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ go get github.com/nfnt/resize
 go get github.com/dennwc/gotrace
 go get github.com/kodeworks/golang-image-ico
 go get golang.org/x/crypto/bcrypt
-go get github.com/360EntSecGroup-Skylar/excelize
+go get github.com/xuri/excelize/v2
 go get golang.org/x/crypto/argon2
 
 echo Building...
