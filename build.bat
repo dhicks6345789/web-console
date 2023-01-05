69c2c768a5e44f1a2a2eb12b76b39aa16dcb6040
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-01-05 16:06:34 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,22 +5,11 @@ erase "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
 erase webconsole.exe > nul 2>&1
 
 echo Checking libraries are installed...
-go install github.com/nfnt/resize@latest
 go get github.com/nfnt/resize
-
-go install github.com/dennwc/gotrace@latest
 go get github.com/dennwc/gotrace
-
-go install github.com/kodeworks/golang-image-ico@latest
 go get github.com/kodeworks/golang-image-ico
-
-go install golang.org/x/crypto/bcrypt@latest
 go get golang.org/x/crypto/bcrypt
-
-go install github.com/360EntSecGroup-Skylar/excelize
 go get github.com/360EntSecGroup-Skylar/excelize
-
-go install golang.org/x/crypto/argon2@latest
 go get golang.org/x/crypto/argon2
 
 echo Building...
