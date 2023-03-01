e75d881d2625d429e5d0bdd90acf959971b87b28
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-03-01 15:23:46 +0000

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -572,10 +572,10 @@ func listFolderAsJSON(thePath string) string {
 	for _, item := range files {
 		if item.IsDir() {
 			result = result + "[\"" + item.Name + "\",\n"
-			result = result + listFolderAsJSON(thePath + "/" + item.Name)
+			result = result + listFolderAsJSON(thePath + "/" + item.Name())
 			result = result + "],"
 		} else {
-			result = result + "\"" + item.Name + "\","
+			result = result + "\"" + item.Name() + "\","
 		}
 	}
 	return result
