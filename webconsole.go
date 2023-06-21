e5743ce73cf3453a6aaa7e715f3882a8b8a3ae3b
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-06-21 12:16:18 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -1385,7 +1385,7 @@ func main() {
 		fmt.Println("Web server available at: http://localhost:" + arguments["port"] + "/")
 		if _, err := os.Stat("/usr/local/bin/ngrok"); err == nil {
 			// ngrokURL, _ := exec.Command("bash -c \"cat /var/log/syslog | grep ngrok.*localhost | tail -1 | cut -d '=' -f 8\"").CombinedOutput()
-			ngrokURL, _ := exec.Command("bash -c \"echo banana\"s").CombinedOutput()
+			ngrokURL, _ := exec.Command("bash", "-c", "\"echo banana\"s").CombinedOutput()
 			fmt.Printf("ngrok URL: %s --nostop\n", ngrokURL)
 		}
 		if arguments["debug"] == "true" {
