1123823e3bfd2521c5a504207d9a96d3ae2a50d8
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-06-21 11:14:45 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ go get github.com/nfnt/resize
 go get github.com/dennwc/gotrace
 go get github.com/kodeworks/golang-image-ico
 go get golang.org/x/crypto/bcrypt
-go get github.com/360EntSecGroup-Skylar/excelize
+# go get github.com/360EntSecGroup-Skylar/excelize
+go get github.com/xuri/excelize/v2
+
 go build -ldflags "-X main.buildVersion=$BUILDVERSION" webconsole.go
 cp webconsole /usr/local/bin
 
