57b32a0dc3072e9fa7d06f201eed9dbd0a1e9293
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-10-25 14:51:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ echo Building Web Console...
 # Stop any existing running service.
 systemctl stop webconsole
 
-#go get github.com/nfnt/resize
-#go get github.com/dennwc/gotrace
-#go get github.com/kodeworks/golang-image-ico
-#go get golang.org/x/crypto/bcrypt
+go get github.com/nfnt/resize
+go get github.com/dennwc/gotrace
+go get github.com/kodeworks/golang-image-ico
+go get golang.org/x/crypto/bcrypt
 ## go get github.com/360EntSecGroup-Skylar/excelize
-#go get github.com/xuri/excelize/v2
+go get github.com/xuri/excelize/v2
 
 go build -ldflags "-X main.buildVersion=$BUILDVERSION" webconsole.go
 cp webconsole /usr/local/bin
