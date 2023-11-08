3cdeab05b973e0277ae015e3d878066e719f4813
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-11-08 19:08:59 +0000

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -1283,7 +1283,8 @@ func main() {
 									for pl := 0; pl < len(logItems); pl = pl + 1 {
 										parsedTimestamp, timestampErr := time.Parse(time.RFC3339, strings.Split(logItems[pl].Name(), "Z")[0]+"Z")
 										if timestampErr == nil {
-											if parsedTimestamp.Unix() < taskStartTimes[taskID] - taskDetails["logPeriod"] {
+											taskLogPeriod, _ = strconv.ParseInt(taskDetails["logPeriod"], 10, 64)
+											if parsedTimestamp.Unix() < taskStartTimes[taskID] - taskLogPeriod {
 												os.Remove(logfilePath + "/" + logItems[pl].Name())
 											}
 										}
