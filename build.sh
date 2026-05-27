e93864d23905c5955f56c5132d0cde63cf837880
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-05-27 14:32:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,45 @@
-# Build script for WebConsole. Tested on Debian, x86 and Raspberry Pi.
+echo "Build script for WebConsole. Tested on Debian, x86 and Raspberry Pi."
+echo "\"build -help\" for more details."
+
+# Set default option values.
+INSTALL=false
+SERVICE=false
+
+# Read user-defined command-line flags.
+while test $# -gt 0; do
+    case "$1" in
+        -install)
+            shift
+            INSTALL=true
+            ;;
+        -service)
+            shift
+            INSTALL=true
+            SERVICE=true
+            ;;
+        -help)
+            echo "Builds the Web Console binary from Go source. Options:"
+            echo "-install"
+            echo "   Installs the binary in /usr/local/bin and supporting files in /etc/webconsole."
+            echo "-service"
+            echo "   Sets Web Console up as a service."
+            exit 1
+            ;;
+        *)
+            echo "$1 is not a recognized flag."
+            exit 1;
+            ;;
+    esac
+done
 
 source VERSION
 CURRENTDATE=`date +"%d/%m/%Y-%H:%M"`
 BUILDVERSION="$VERSION-local-$CURRENTDATE"
 
-echo Building Web Console...
-
-# Stop any existing running service.
-systemctl stop webconsole
+if [ $SERVICE = true ]; then
+  # Stop any existing running service.
+  systemctl stop webconsole
+fi
 
 go get github.com/nfnt/resize
 go get github.com/dennwc/gotrace
@@ -25,20 +57,24 @@ go build -ldflags "-X main.buildVersion=$BUILDVERSION" webconsole.go
 # Exit if we didn't manage to build the executable.
 [ ! -f webconsole ] && { echo "Error: webconsole not compiled."; exit 1; }
 
-# Install the new executable in place.
-cp webconsole /usr/local/bin
+if [ $INSTALL = true ]; then
+  # Install the new executable in place.
+  cp webconsole /usr/local/bin
 
-# Create the application's data folder and copy the default data files into it.
-[ ! -d /etc/webconsole ] && mkdir /etc/webconsole
-[ ! -d /etc/webconsole/tasks ] && mkdir /etc/webconsole/tasks
-[ ! -d /etc/webconsole/www ] && mkdir /etc/webconsole/www
-[ ! -d /etc/webconsole/www/ace ] && mkdir /etc/webconsole/www/ace
-cp -r www/* /etc/webconsole/www
-cp -r ../ace-builds/src-noconflict/* /etc/webconsole/www/ace
+  # Create the application's data folder and copy the default data files into it.
+  [ ! -d /etc/webconsole ] && mkdir /etc/webconsole
+  [ ! -d /etc/webconsole/tasks ] && mkdir /etc/webconsole/tasks
+  [ ! -d /etc/webconsole/www ] && mkdir /etc/webconsole/www
+  [ ! -d /etc/webconsole/www/ace ] && mkdir /etc/webconsole/www/ace
+  cp -r www/* /etc/webconsole/www
+  cp -r ../ace-builds/src-noconflict/* /etc/webconsole/www/ace
+fi
 
-# Set up systemd to run Webconsole, if it isn't already.
-[ ! -f /etc/systemd/system/webconsole.service ] && cp webconsole.service /etc/systemd/system/webconsole.service && chmod 644 /etc/systemd/system/webconsole.service
+if [ $SERVICE = true ]; then
+  # Set up systemd to run Webconsole, if it isn't already.
+  [ ! -f /etc/systemd/system/webconsole.service ] && cp webconsole.service /etc/systemd/system/webconsole.service && chmod 644 /etc/systemd/system/webconsole.service
 
-# Restart the webconsole service.
-systemctl start webconsole
-systemctl enable webconsole
+  # Restart the webconsole service.
+  systemctl start webconsole
+  systemctl enable webconsole
+fi
