d26004950a8829d3837a3bce2999a23a146b455e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-05-28 17:57:34 +0100

    Update webconsole.go

--- webconsole.go
+++ webconsole.go
@@ -970,7 +970,7 @@ func main() {
 			}
 		}
 	}
-	debug("authServicesUsed: " + strings.Join(authServicesUsed, ",") + "."
+	debug("authServicesUsed: " + strings.Join(authServicesUsed, ",") + ".")
 	
 	if arguments["start"] == "true" {
 		// Start the thread that checks for and clears expired tokens.
